80628b7dbd925224cee0f2550225180311ff7adc
aditissigh24 <aditissigh24@gmail.com>
2024-11-12 20:14:35 +0530

    updated the anagram function

---
+++ 01-js/easy/anagram.js
@@ -0,0 +1,18 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function isAnagram(str1, str2) {
+const normalize = (str)=> str.replace(/\s+/g,'').toLowerCase().split('').sort().join('');
+if (normalize(str1)===normalize(str2)){
+  console.log('true');
+}
+}
+
+//export default isAnagram;
+let a="silent";
+let b="listen";
+let c=isAnagram(a,b);
+
