02ff4c4ea3481daad77792a2186de0b082634157
aditissigh24 <aditissigh24@gmail.com>
2024-11-28 03:37:28 +0530

    did week-03 01-03

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
+const { error } = require('console');
 
 const app = express();
 let errorCount = 0;
@@ -9,10 +10,31 @@ let errorCount = 0;
 // Your task is to
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+app.use((req,res,next)=>{
+  if(err){
+    errorCount+=1;
+    return res.status(400).json({error:err.message || "not found"})
+  }
+  next();
+})
 
+app.use((req,res,next)=>{
+  const userID=req.header('user-id')
+  if(!userID){
+    res.status(400).json("userid should be in header")
+    errorCount= errorCount+1
+  }
+})
 app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+  try{
+    throw new Error("User not found");
+    res.status(200).json({ name: 'john' });
+  }
+  catch(err){
+    errorCount+=1;
+    next(err)
+  }
+  
 });
 
 app.post('/user', function(req, res) {
